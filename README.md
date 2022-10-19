2029c909f1837681189306972357f1c4efb98135
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2022-10-18 18:23:44 -0700

    final commit

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# Module5Challenge
 # Work Day Scheduler Starter Code
 
+## User Story
 
+As a web developer, I need a scheduler that is capable of saving important events and dates to local storage
+
+## Acceptance Criteria
+
+It is done 
+
+WHEN the scheduler is open it shows the current date with a box, once the box is clicked, a calendar will pop up and you can select the date of the event
+
+THEN you can select a time within the color-coded boxes, and add an event to the text area
+
+WHEN you enter an event
+
+THEN you click the save button and the event is saved to local stotrage
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,37 +2,56 @@
 var dateDisplayEl = $('#date-display');
 var eventNameEl = $('input[event="event-name"]');
 
+var saveBtn = document.getElementById("save");
+
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
-// TODO: 4. What is the current time in the format: hours:minutes:seconds
 
 // Datepicker widget
 $(function () {
     $('#datepicker').datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
+        changeMonth: true,
+        changeYear: true,
+    })
 
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-$('#event-display').children('ul').children().addClass('bg-primary text-dark mb-3 p-3').css('border-radius', '.4rem');
+// $(function () {
+//     $('#dialog').dialog(
+//         $(function () {
+//             $('#datepicker').datepicker({
+//                 changeMonth: true,
+//                 changeYear: true,
+//             });
 
-// WHEN I click into a time block
+//         })
+//     )
+    // WHEN I view the time blocks for that day
+    // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+    // WHEN I click into a time block
 
 
 
-// THEN I can enter an event
-function newEvent(event) {
+    // THEN I can enter an event
+    function newEvent(event) {
         event.preventDefault();
 
         console.log('event-Name', eventNameEl.val());
+
+
+        // WHEN I click the save button for that time block
+        saveBtn.addEventListener("click", function (event) {
+            event.preventDefault();
+
+        }
+
+            // THEN the text for that event is saved in local storage
+
+            // WHEN I refresh the page
+            // THEN the saved events persist
+        )
     }
 
-    // WHEN I click the save button for that time block
-    // THEN the text for that event is saved in local storage
-    // WHEN I refresh the page
-    // THEN the saved events persist
-})
+});
